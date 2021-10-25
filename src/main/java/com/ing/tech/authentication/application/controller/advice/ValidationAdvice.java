d01f15bf313d29492908b782cd14cd9dd0074b8b
andreea-PC\andreea <andras.hik@gmail.com>
2021-10-25 22:31:37 +0300

    Add controller advice and initial method for registering a user

---
+++ src/main/java/com/ing/tech/authentication/application/controller/advice/ValidationAdvice.java
@@ -0,0 +1,40 @@
+package com.ing.tech.authentication.application.controller.advice;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ValidationAdvice extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException ex) {
+    List<Object> errors = new ArrayList();
+    ex.getConstraintViolations().forEach(constraintViolation -> errors
+        .add(constraintViolation.getPropertyPath() + ":" + constraintViolation.getMessage()));
+    log.error(errors.toString());
+
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+      HttpHeaders headers, HttpStatus status, WebRequest request) {
+    List<Object> errors = new ArrayList();
+    ex.getBindingResult().getFieldErrors().forEach(
+        fieldError -> errors.add(fieldError.getField() + ":" + fieldError.getDefaultMessage()));
+    log.error(errors.toString());
+
+    return handleExceptionInternal(ex, errors.toString(), headers, status, request);
+  }
+}
--- src/main/java/com/ing/tech/authentication/application/service/RegistrationService.java
+++ src/main/java/com/ing/tech/authentication/application/service/RegistrationService.java
@@ -1,5 +1,6 @@
 package com.ing.tech.authentication.application.service;
 
+import com.ing.tech.authentication.application.model.RegistrationRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +8,6 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class RegistrationService {
 
+  public void registerUser(RegistrationRequestDto registrationRequestDto) {
+  }
 }
