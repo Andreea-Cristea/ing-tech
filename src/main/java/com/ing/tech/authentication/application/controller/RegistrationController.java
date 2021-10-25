8bb765093fcc36f147fca06a01b09aff31756872
andreea-PC\andreea <andras.hik@gmail.com>
2021-10-25 20:28:48 +0300

    Create RegistrationController class

---
+++ src/main/java/com/ing/tech/authentication/application/controller/RegistrationController.java
@@ -0,0 +1,12 @@
+package com.ing.tech.authentication.application.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+}
