60e163189e09ae0fd054cb01559e8da1bbb8d6a2
andreea-PC\andreea <andras.hik@gmail.com>
2021-10-25 20:38:01 +0300

    Create RegistrationService

---
+++ src/main/java/com/ing/tech/authentication/application/service/RegistrationService.java
@@ -0,0 +1,10 @@
+package com.ing.tech.authentication.application.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+}
