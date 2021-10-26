77816a62b26b21e3556ca431f8e874ef3b855188
andreea-PC\andreea <andras.hik@gmail.com>
2021-10-26 23:18:30 +0300

    Remove unused import from UserMapper

--- src/main/java/com/ing/tech/authentication/domain/mapper/UserMapper.java
+++ src/main/java/com/ing/tech/authentication/domain/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.ing.tech.authentication.domain.mapper;
 import com.ing.tech.authentication.application.model.RegistrationRequestDto;
 import com.ing.tech.authentication.domain.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper
 public interface UserMapper {
