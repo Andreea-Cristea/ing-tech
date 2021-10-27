0798b223c25a2cc2bd979a560e8ca761780e10bc
andreea-PC\andreea <andras.hik@gmail.com>
2021-10-27 21:43:52 +0300

    Add the dev yml file in which the requests to the unsecured endpoints can be tested without the form authentication and the h2 console is enabled and add the SecurityProductionConfig in which the h2 console is not accessible and the csrf is enabled

---
+++ src/main/java/com/ing/tech/authentication/application/security/SecurityProductionConfig.java
@@ -0,0 +1,65 @@
+package com.ing.tech.authentication.application.security;
+
+import com.ing.tech.authentication.application.service.UserAuthenticationManager;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Slf4j
+@Profile("prod")
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityProductionConfig extends WebSecurityConfigurerAdapter {
+  private final UserAuthenticationManager userAuthenticationManager;
+  private final BCryptPasswordEncoder bCryptPAsswordEncoder;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers("/api/v1/registration")
+        .permitAll()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/api/v1/login").permitAll()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/home")
+        .permitAll()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/info").hasAnyAuthority("USER")
+        .and()
+        .authorizeRequests().antMatchers("/console/**").permitAll()
+        .and()
+        .logout(logout -> logout.logoutUrl("/api/v1/logout")
+            .addLogoutHandler((request, response, auth) -> {
+              try {
+                request.logout();
+                response.sendRedirect("/home");
+              } catch (ServletException | IOException e) {
+                log.error(e.getMessage());
+              }
+            }))
+        .authorizeRequests()
+        .anyRequest()
+        .authenticated().and()
+        .formLogin();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder)
+      throws Exception {
+    authenticationManagerBuilder.userDetailsService(userAuthenticationManager)
+        .passwordEncoder(bCryptPAsswordEncoder);
+  }
+}
